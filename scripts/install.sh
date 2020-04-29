5e177098a9abfe491064a464bc64ee4e77967db9
Thomas Kälin <kaelin@sweb.ch>
2020-04-29 16:37:27 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,14 +7,21 @@
 # siehe https://websiteforstudents.com/install-apache2-mariadb-and-php-7-2-with-phpmyadmin-on-ubuntu-16-04-18-04-18-10-lamp-phpmyadmin/
 sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove
 sudo apt-get install debconf-utils
-sudo apt-get install -y apache2 php libapache2-mod-php vsftpd markdown mariadb-server mariadb-client
+sudo apt-get install -y apache2 vsftpd markdown 
+# php libapache2-mod-php 
+
+sudo apt-get install mariadb-server mariadb-client
+
+sudo systemctl stop apache2.service
+sudo systemctl start apache2.service
+sudo systemctl enable apache2.service
+
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
-# update to PHP 7.2-FPM
+
 sudo apt-get update
 sudo apt-get install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
-
-sudo apt-get install php-mbstring php-zip php-gd
+sudo apt-get install php-mbstring php-gettext
 
 # identical passwords to make handling easier
 APP_PASS="passw0rd"
@@ -30,10 +37,7 @@ echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo de
 # nötig?
 echo "phpmyadmin phpmyadmin/internal/skip-preseed boolean true" | sudo debconf-set-selections
 
-sudo apt-get install -y phpmyadmin php-mbstring php-gettext
-
-sudo phpenmod mcrypt
-sudo phpenmod mbstring
+sudo apt-get install -y phpmyadmin 
 sudo systemctl apache2 restart
 
 # Home Verzeichnis unter http://<host>/data/ verfuegbar machen
