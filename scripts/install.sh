ab56c2499a3f46dbfad9b57255190a13f65edb92
Thomas Kälin <kaelin@sweb.ch>
2020-04-29 15:39:33 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,6 +14,8 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
 
+sudo apt-get install php-mbstring php-zip php-gd
+
 # identical passwords to make handling easier
 APP_PASS="passw0rd"
 ROOT_PASS="passw0rd"
@@ -24,8 +26,15 @@ echo "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS" | sudo debc
 echo "phpmyadmin phpmyadmin/mysql/admin-pass password $ROOT_PASS" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS" | sudo debconf-set-selections
 echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo debconf-set-selections
+
+# nötig?
+echo "phpmyadmin phpmyadmin/internal/skip-preseed boolean true" | sudo debconf-set-selections
+
 sudo apt-get install -y phpmyadmin php-mbstring php-gettext
 
+sudo phpenmod mcrypt
+sudo phpenmod mbstring
+sudo systemctl apache2 restart
 
 # Home Verzeichnis unter http://<host>/data/ verfuegbar machen
 mkdir -p /home/ubuntu/data/
