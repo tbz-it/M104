5b35a75f5a769a2e113a6fa2e29dc789bf56a9f4
Thomas Kälin <kaelin@sweb.ch>
2020-05-06 16:53:44 +0200

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,35 +2,33 @@
 #   
 #   Installiert den Apache Web Server
 #
-export KUBECONFIG=$HOME/.kube/config
-kubectl apply -f ~/M104/mysql.yaml
-kubectl apply -f ~/M104/phpmyadmin.yaml
-exit 0
+# export KUBECONFIG=$HOME/.kube/config
+# kubectl apply -f ~/M104/mysql.yaml
+# kubectl apply -f ~/M104/phpmyadmin.yaml
+# exit 0
 
 # Install apache, MariaDB Database Server, php, ftp, powershell, markdown to HTML
 # siehe https://websiteforstudents.com/install-apache2-mariadb-and-php-7-2-with-phpmyadmin-on-ubuntu-16-04-18-04-18-10-lamp-phpmyadmin/
 sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove
-sudo apt-get install debconf-utils
-exit 0
-
-sudo apt-get install -y apache2 vsftpd markdown 
+sudo apt-get install -y debconf-utils
+sudo apt-get install -y apache2 
 # php libapache2-mod-php 
 
 echo "MariaDB is getting installed..."
-sudo apt-get install mariadb-server mariadb-client
+sudo apt-get install -y mariadb-server mariadb-client
 
 sudo systemctl stop apache2.service
 sudo systemctl start apache2.service
 sudo systemctl enable apache2.service
 
 echo "Repository is getting added..."
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
 echo "PHP is getting installed..."
 sudo apt-get update
-sudo apt-get install php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
-sudo apt-get install php-mbstring php-gettext
+sudo apt-get install -y php7.2-fpm php7.2-common php7.2-mbstring php7.2-xmlrpc php7.2-soap php7.2-gd php7.2-xml php7.2-intl php7.2-mysql php7.2-cli php7.2-zip php7.2-curl
+sudo apt-get install -y php-mbstring php-gettext
 
 echo "Setting some variables to make the installation unattended..."
 # identical passwords to make handling easier
@@ -48,7 +46,7 @@ echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | sudo de
 # echo "phpmyadmin phpmyadmin/internal/skip-preseed boolean true" | sudo debconf-set-selections
 
 echo "Installing phpmyadmin..."
-sudo apt-get install -y phpmyadmin 
+sudo apt-get -y install -y phpmyadmin 
 
 echo "Defining a virtual host..."
 # siehe https://unix.stackexchange.com/questions/190549/command-for-setting-documentroot-for-apache-on-debian
